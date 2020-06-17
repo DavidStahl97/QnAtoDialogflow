ddf9de5120e0464911fcc9beda6ec97f62eb8e7c
de18702 <stahl.d@loh-services.de>
2020-06-17 12:32:20 +0200

    Change to .NET Framework

---
+++ QnaDialogflowConverter/ClosedXMLReader.cs
@@ -0,0 +1,70 @@
+﻿using ClosedXML.Excel;
+using LTRSimulation.Core.Files;
+using System;
+using System.Linq;
+
+namespace LTRSimulation.Common.Services
+{
+    public class ClosedXmlTable : IExcelReader, IDisposable
+    {
+        private readonly XLWorkbook _workbook;
+        private readonly IXLWorksheet _worksheet;
+
+        private bool _disposed;
+
+        public ClosedXmlTable(string tableName)
+        {
+            _workbook = new XLWorkbook(tableName);
+            _worksheet = _workbook.Worksheet(1);
+
+            RowCount = _worksheet.RowsUsed().Count();
+        }
+
+        ~ClosedXmlTable()
+        {
+            Dispose(false);
+        }
+
+        public int RowCount { get; }
+
+        public DateTime GetDateTime(int row, int column)
+        {
+            var r = _worksheet.Row(row);
+            var cell = r.Cell(column);
+
+            var dateTime = cell.GetValue<DateTime>();
+            return dateTime;
+        }
+
+        public string GetNumber(int row, int column)
+        {
+            return GetStringOfField(row, column);
+        }
+
+        public string GetStringOfField(int row, int column)
+        {
+            return _worksheet.Row(row).Cell(column).GetValue<string>();
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposed)
+            {
+                return;
+            }
+
+            if (disposing)
+            {
+                _workbook?.Dispose();
+            }
+
+            _disposed = true;
+        }
+    }
+}
--- QnaDialogflowConverter/Program.cs
+++ QnaDialogflowConverter/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace QnaDialogflowConverter
 {
     class Program
     {
+        [STAThread]
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var openFile = new OpenFileDialog
+            {
+                Filter = "Excel Files (*.xlsx)|*.xlsx",
+                Title = "QnA File"
+            };
+
+            var result = openFile.ShowDialog();
+            if (result != DialogResult.OK)
+            {
+                Console.WriteLine("Choose QnA file");
+                return;
+            }
+
+            var qnaFile = openFile.FileName;
+
+
         }
     }
 }
